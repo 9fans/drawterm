e67eaf09f50f9cdd2d72f08ee472682a9edbdd6f
Adrian Grigore <i@adi.onl>
2024-06-23 03:49:05 +0300

    libauthsrv: sync with Plan 9

--- libauthsrv/authdial.c
+++ libauthsrv/authdial.c
@@ -7,25 +7,30 @@
 int
 authdial(char *netroot, char *dom)
 {
-	char server[Ndbvlen];
-	Ndbtuple *nt;
+	char *p;
+	int rv;
 
-	
-	if(dom != nil){
-		/* look up an auth server in an authentication domain */
-		nt = csgetval(netroot, "authdom", dom, "auth", server);
-
-		/* if that didn't work, just try the IP domain */
-		if(nt == nil)
-			nt = csgetval(netroot, "dom", dom, "auth", server);
-		if(nt == nil){
-			werrstr("no auth server found for %s", dom);
-			return -1;
-		}
-		ndbfree(nt);
-		return dial(netmkaddr(server, netroot, "ticket"), 0, 0, 0);
-	} else {
+	if(dom == nil)
 		/* look for one relative to my machine */
 		return dial(netmkaddr("$auth", netroot, "ticket"), 0, 0, 0);
+
+	/* look up an auth server in an authentication domain */
+	p = csgetvalue(netroot, "authdom", dom, "auth", nil);
+
+	/* if that didn't work, just try the IP domain */
+	if(p == nil)
+		p = csgetvalue(netroot, "dom", dom, "auth", nil);
+	/*
+	 * if that didn't work, try p9auth.$dom.  this is very helpful if
+	 * you can't edit /lib/ndb.
+	 */
+	if(p == nil)
+		p = smprint("p9auth.%s", dom);
+	if(p == nil){			/* should no longer ever happen */
+		werrstr("no auth server found for %s", dom);
+		return -1;
 	}
+	rv = dial(netmkaddr(p, netroot, "ticket"), 0, 0, 0);
+	free(p);
+	return rv;
 }
